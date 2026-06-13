670b7a475df64263c2f518a431adbe0edb7f3dd3
Adrian Yadao <adrianmiguelyadao@gmail.com>
2026-06-13 16:58:38 -0600

    saturday python practice

--- python/main.py
+++ python/main.py
@@ -1,3 +1,5 @@
+# name = input("Enter your name:")
+# print(name)
 
 # name = input("Enter Your Name")
 # print("Hello " + name)
@@ -32,3 +34,40 @@
 # def get_length(list):
 #     return len(list)
 # print(get_length(my_list))
+
+# items = ["one", "two", "three"]
+# def print_items(items):
+#     for i, item in enumerate(items, 1):
+#         print(f'{i}: {item}')
+# print_items(items)
+
+# number1 = int (input ("Enter number 1 here:"))
+# number2 = int (input ("Enter number 2 here:"))
+# sum = number1 + number2
+# print(sum)
+
+# def is_positive(number):
+#     if number > 0:
+#         return  True
+#     else:
+#         return False
+# print(is_positive(-5))
+
+
+# items = ["Crystal Ball", "Talisman", "Cursed Mirror", "Demon Key", "Cursed Locket"]
+
+# for item in items:
+#     if item.startswith("C"):
+#         print(item)
+
+
+# def count_vowels(name):
+#     vowels = ["a", "e", "i", "o", "u"]
+#     count = 0
+#     for char in name:
+#         if char in vowels:
+#             count += 1
+#     return count
+
+# print(count_vowels("Adrian"))
+
