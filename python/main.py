013ec9d54b9d4ab13562676f55f4ca31eae40fbe
Adrian Yadao <adrianmiguelyadao@gmail.com>
2026-06-13 15:18:16 -0600

    recap

---
+++ python/main.py
@@ -0,0 +1,34 @@
+
+# name = input("Enter Your Name")
+# print("Hello " + name)
+# print (f"Hello {name}")
+
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print ("You're allowed to vote.")
+# else:
+#     print ("You're not allowed to vote.")
+
+# for i in range(1, 6):
+#     print(i)
+
+# number = int (input ("Enter a number:"))
+# while number != 0:
+#     number = int (input ("Enter a number:"))
+# print("done")
+
+# def multiply(a, b):
+#     return a * b
+# print(multiply(4,5))
+# result = multiply(4, 5)  # stored because we used return
+# print(result)  # now we can use it
+
+# def greet(name):
+#     print(f"Hello {name}")
+
+# greet("Adrian")
+
+# my_list = ["a", "b", "c"]
+# def get_length(list):
+#     return len(list)
+# print(get_length(my_list))
