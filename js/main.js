390e1f043d5903297c97a6fd5db832b6796ee0c5
Adrian Yadao <adrianmiguelyadao@gmail.com>
2026-06-20 15:57:45 -0600

    js bubbling,captures and targets

--- js/main.js
+++ js/main.js
@@ -44,3 +44,67 @@
 // };
 
 // reverseString("HELLO");
+
+// const btn = document.querySelector("#btn");
+
+// const greetUser = (uName) => {
+//   return `Hello ${uName}!`;
+// };
+
+// greetUser("Adrian");
+
+// const multiply = (num1, num2) => {
+//   return num1 * num2;
+// };
+
+// console.log(multiply(6, 7));
+
+// const isAdult = (age) => {
+//   if (age >= 18) return true;
+//   else return false;
+// };
+
+// console.log(isAdult(12));
+
+// const sumArray = (numbers) => {
+//   let total = 0;
+//   for (const number of numbers) total += number;
+//   return total;
+// };
+
+// console.log(sumArray([1, 2, 3, 4, 5]));
+
+// const getLastItem = (numbers) => {
+//   const lastNumber = numbers.at(-1);
+//   return lastNumber;
+// };
+
+// console.log(getLastItem(["one", "two", "three"]));
+
+// const reverseString = (word) => {
+//   const reversedWord = word.split("").reverse().join("");
+//   return reversedWord;
+// };
+
+// console.log(reverseString("HELLO"));
+
+// const log = document.getElementById("log");
+// const outer = document.getElementById("outer");
+// const inner = document.getElementById("inner");
+// const button = document.getElementById("btn-propagate");
+
+// const outerClick = (e) => {
+//   log.textContent += " Outer clicked | ";
+//   //   e.stopPropagation();
+// };
+// const innerClick = () => {
+//   log.textContent += " Inner clicked | ";
+// };
+
+// const buttonClick = (e) => {
+//   log.textContent += " Button clicked |";
+//   //   e.stopPropagation();
+// };
+// outer.addEventListener("click", outerClick, { capture: true });
+// inner.addEventListener("click", innerClick);
+// button.addEventListener("click", buttonClick);
